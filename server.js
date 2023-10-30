f07250f14c408c77c303dc1291a42cf7b7979d1b
Teoh Wei Jian <weijianteoh2000@graduate.utm.my>
2023-10-30 23:59:05 +0800

    Finding...2

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const local = '127.0.0.1';
 const host = 'https://apivideobackend.onrender.com';
 
 const hostname = host;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
@@ -24,9 +25,9 @@ const server = http.createServer((req, res) => {
     }
 });
 
-// server.listen(hostname, () => {
-//     console.log(`Server running at http://${hostname}/`);
-// });
+server.listen(port, () => {
+    console.log(`Server running at http://${hostname}/`);
+});
 
 async function handleGetStream(req, res) {
     // retrieve the first page of all livestreams
