0f41a5820bb7725e1d82bef2306beb1bf97720f8
Teoh Wei Jian <weijianteoh2000@graduate.utm.my>
2023-10-31 20:51:26 +0800

    add addstream and deletestream

--- apiVideoNodeJS/server.js
+++ apiVideoNodeJS/server.js
@@ -4,32 +4,28 @@ const ApiVideoClient = require('@api.video/nodejs-client');
 
 const client = new ApiVideoClient({ apiKey: "NkaLmydeIOtdBlVKuayXJ5rQjxHtIMGnEgBAnHgrE9V" });
 
-const local = '127.0.0.1';
-const host = 'https://apivideobackend.onrender.com';
-
-const hostname = host;
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname;
 
     if (pathname === '/getStream') {
-        handleGetStream(req, res);
+        handleGetStream(req, res, parsedUrl.query);
     } else if (pathname === '/addStream') {
-        handleAddStream(req, res);
+        handleAddStream(req, res, parsedUrl.query);
     } else if (pathname === '/deleteStream') {
-        handleDeleteStream(req, res);
+        handleDeleteStream(req, res, parsedUrl.query);
     } else {
         handleNotFound(req, res);
     }
 });
 
 server.listen(port, () => {
-    console.log(`Server running at http://${hostname}/`);
+    console.log(`Server running at port: ${port}`);
 });
 
-async function handleGetStream(req, res) {
+async function handleGetStream(req, res, query) {
     // retrieve the first page of all livestreams
     const liveStreams = await client.liveStreams.list({});
 
@@ -51,12 +47,19 @@ async function handleGetStream(req, res) {
         pageSize: 30
     });
 
+    const responseData = {
+        data: {
+            liveStreams
+        },
+        status: "SUCCESS"
+    };
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ liveStreams }));
+    res.end(JSON.stringify({ responseData }));
 }
 
-async function handleAddStream(req, res) {
+async function handleAddStream(req, res, query) {
     const liveStreamCreationPayload = {
         name: "My Live Stream", // Add a name for your live stream here.
         _public: true, // Whether your video can be viewed by everyone, or requires authentication to see it. 
@@ -64,23 +67,47 @@ async function handleAddStream(req, res) {
 
     const liveStream = await client.liveStreams.create(liveStreamCreationPayload);
 
+    const responseData = {
+        data: {
+            liveStream
+        },
+        status: "SUCCESS"
+    };
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ message: 'Response for Route 2' }));
 }
 
-async function handleDeleteStream(req, res) {
-    const liveStreamId = 'li400mYKSgQ6xs7taUeSaEKr'; // The unique identifier of the live stream whose thumbnail you want to delete.
+async function handleDeleteStream(req, res, query) {
+    if(query.id == null){
+        handleNotFound(req, res)
+    }
+    const liveStreamId = query.id;
 
     const liveStream = await client.liveStreams.delete(liveStreamId);
 
+    const responseData = {
+        data: {
+            liveStream
+        },
+        status: "SUCCESS"
+    };
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ message: 'Response for Route 3' }));
+    res.end(JSON.stringify({responseData}));
 }
 
 function handleNotFound(req, res) {
+    const responseData = {
+        data: {
+            message: 'Wrong URL'
+        },
+        status: "FAIL"
+    };
+
     res.statusCode = 404;
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ message: 'Wrong URL' }));
+    res.end(JSON.stringify({responseData}));
 }
